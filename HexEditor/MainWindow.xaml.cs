7750cd8101dac6d6d9890313f81c747a07865b6e
Vyacheslav <geoft.biryukov@gmail.com>
2019-09-12 23:57:32 +0400

    Хостинг PropertyGrid

---
+++ HexEditor/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HexEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly ReadBytesOptions options = new ReadBytesOptions();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+           (WfHost.Child as PropertyGrid).SelectedObject = options;
+
+            var commandBinding = new CommandBinding
+            {
+                Command = ApplicationCommands.Open
+            };
+            commandBinding.Executed += OpenFileCommandExecuted;
+            OpenMenuItem.CommandBindings.Add(commandBinding);
+        }
+
+        private void OpenFileCommandExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            System.Windows.MessageBox.Show("Open File");
+        }
+    }
+}
---
+++ HexEditor/ReadBytesOptions.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HexEditor
+{
+    class ReadBytesOptions : INotifyPropertyChanged
+    {
+        #region File Info Category
+        private const string fileInfoCategory = "Info";
+
+        private string fileName = string.Empty;
+        [Category(fileInfoCategory)]
+        [ReadOnly(true)]
+        public string FileName
+        {
+            get => fileName;
+            set
+            {
+                Set(nameof(FileName), ref fileName, value);
+            }
+        }
+
+        private string fileSize = "0";
+        [Category(fileInfoCategory)]
+        [ReadOnly(true)]
+        public string FileSize
+        {
+            get => fileSize;
+            set
+            {
+                Set(nameof(FileSize), ref fileSize, value);
+            }
+        }
+
+        #endregion
+
+        #region Read Category
+        private const string readCategory = "Read";
+
+        private long offset = 0;
+        [Category(readCategory)]
+        public long Offset
+        {
+            get => offset;
+            set
+            {
+                Set(nameof(Offset), ref offset, Math.Min(value, maxBytesToRead));
+            }
+        }
+
+        private long bytesToRead = 1024;
+        [Category(readCategory)]
+        public long BytesToRead
+        {
+            get => bytesToRead;
+            set
+            {
+
+                Set(nameof(BytesToRead), ref bytesToRead, Math.Min(value, maxBytesToRead - offset));
+            }
+        }
+        #endregion
+
+        #region View Category
+        private const string viewCategory = "View";
+
+        private int bytesInRow = 20;
+        [Category(viewCategory)]
+        public int BytesInRow
+        {
+            get => bytesInRow;
+            set
+            {
+                Set(nameof(BytesInRow), ref bytesInRow, value);
+            }
+        }
+        #endregion
+
+        private void Set<T>(string propertyName, ref T field, T value)
+        {
+            field = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private long maxBytesToRead = 0;
+        public void SetMaxBytes(long length)
+        {
+            maxBytesToRead = length;
+        }
+
+    }
+}
